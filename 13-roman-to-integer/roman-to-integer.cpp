b8002d70462ba76879df79b7eccf5064682f6a80
Yash Kashyap <kashyapyash592@gmail.com>
2026-03-25 21:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (93.91%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> m = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-            {'C', 100}, {'D', 500}, {'M', 1000}
-        };
-        
         int ans = 0;
         int n = s.length();
         
         for (int i = 0; i < n; i++) {
-            // If current value is less than the next value, subtract it
-            if (i < n - 1 && m[s[i]] < m[s[i+1]]) {
-                ans -= m[s[i]];
+            int curr = getValue(s[i]);
+            
+            // Check if the next numeral exists and is larger
+            if (i + 1 < n && curr < getValue(s[i+1])) {
+                ans -= curr;
             } else {
-                // Otherwise, add it
-                ans += m[s[i]];
+                ans += curr;
             }
         }
-        
         return ans;
+    }
+    private:
+    inline int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 };
